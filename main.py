ff49f1a1a0264b62dd1ff873574ea70bd7f39591
DeepxHyeon <deepxhyeon@gmail.com>
2020-06-19 22:13:26 +0900

    DB 연동, 효과음 적용

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import random
 import time
+import sqlite3
+import datetime
+import pygame.mixer
+
+
+conn = sqlite3.connect('/Users/deepxhyeon/typing-game/resource/records.db', isolation_level = None)
+cursor = conn.cursor()
+cursor.execute(
+    "CREATE TABLE IF NOT EXISTS records(id INTEGER PRIMARY KEY AUTOINCREMENT, cor_cnt INTEGER, record text, regdate text)"
+)
 
 words = []
 
 n = 1
 cor_cnt = 0
 
-with open('./resource/word.txt', 'r') as f:
+with open('/Users/deepxhyeon/typing-game/resource/word.txt', 'r') as f:
     for c in f:
         words.append(c.strip())
 # print(words)
@@ -25,15 +35,20 @@ while n <= 10:
     print(q)
 
     x = input()
-
+ 
     print()
+
+    pygame.mixer.init()
 
     if str(q).strip() == str(x).strip():
         print("Pass!")
+        gs = pygame.mixer.Sound("/Users/deepxhyeon/typing-game/sound/good.wav")
+        gs.play()
         cor_cnt += 1
     else:
         print("Wrong!")
-
+        bs = pygame.mixer.Sound("/Users/deepxhyeon/typing-game/sound/bad.wav")
+        bs.play()
     n += 1
 
 end = time.time()
@@ -44,6 +59,8 @@ if cor_cnt >= 6:
     print("\n합격")
 else:
     print("\n불합격")
+
+cursor.execute("INSERT INTO records('cor_cnt', 'record', 'regdate') VALUES (?, ?, ?)", (cor_cnt, et, datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
 
 print("< 게임 시간 :", et, "초 /", "정답 개수 : {} >".format(cor_cnt))
 
